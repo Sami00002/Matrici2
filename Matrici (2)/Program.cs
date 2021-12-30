db18f73f51320603aea4f53cdfe7a3f5c8c19a76
Sami00002 <samuel.bodogan@emanuel.ro>
2021-12-30 14:42:05 +0200

    Add project files.

---
+++ Matrici (2)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Matrici__2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Dimensiunea matricei (x) :");
+            int nr = Int32.Parse(Console.ReadLine());
+            Console.Write("Dimensiunea matricei (y) :");
+            int nr2 = Int32.Parse(Console.ReadLine());
+            int[,] matrix = new int[nr2, nr];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"matrix[{row},{col}]:");
+                    matrix[row, col] = Int32.Parse(Console.ReadLine());
+
+                }
+            }
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+
+                    Console.Write(matrix[row, col] + "\t");
+
+
+
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
